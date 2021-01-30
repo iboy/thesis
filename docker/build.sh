b37959aecb9c2b1bbabf5ee8d43a6e50727eaeef
Ian Grant <ian.grant@mac.com>
2021-01-30 22:28:30 +0000

    Keep it going

---
+++ docker/build.sh
@@ -0,0 +1,79 @@
+#!/bin/sh
+
+ROOT="$PWD/src"
+OUTPUT="$PWD/output"
+TEXTBUNDLE="$ROOT/thesis.textbundle"
+ASSETS="$TEXTBUNDLE/assets"
+TEMPLATE="$ROOT/template"
+
+# Move into a folder if it exists,
+# else abort the program with error
+safe_cd() {
+  cd "$1" || (fail_echo "$1 not found.")
+}
+
+# Echo in rex text and abort
+fail_echo() {
+  echo "\e[41m$1\e[39m"
+  exit 1
+}
+
+# Echo in green text
+blue_echo() {
+  echo "\e[34m$1\e[39m"
+}
+
+cleanup() {
+  # Remove Pandoc Output
+  safe_cd "$TEXTBUNDLE"
+  rm -f -- *.tex *.tex-e
+
+  # Remove convertd images
+  safe_cd "$ASSETS"
+  rm -f -- *-converted.jpg *-converted-*.jpg
+
+  # Remove LaTeX Output
+  safe_cd "$TEMPLATE"
+  rm -f -- *.pdf *.aux *.fdb_latexmk *.fls *.lof *.lot *.log *.out *.synctex.gz *.toc *.bcf *.blg *.dvi *.run.xml *.out.ps
+}
+
+# First, clean up
+blue_echo "Cleaning directories..."
+cleanup
+
+# Build TeX from Markdown
+blue_echo "Building TeX from Markdown..."
+safe_cd "$TEXTBUNDLE"
+pandoc --filter pandoc-crossref --filter pandoc-citeproc \
+  -M cref=true \
+  --bibliography ../bibliography/bibliography.bib --biblatex \
+  --top-level-division=chapter \
+  -o text.tex text.md
+
+# Convert TIFF Images to JPG
+blue_echo "Converting images that are not compatible with pdfLaTeX..."
+safe_cd "$ASSETS"
+for f in *.tiff *.tif
+do
+    if [ ! -f "$f" ]; then
+      continue
+    fi
+
+    filename="$(basename -- "$f")-converted.jpg"
+    echo "Converting $f"
+    convert "$f" -flatten "$filename" && sed -i -e "s|$f|$filename|g" ../text.tex
+done
+
+# Build PDF from TeX
+blue_echo "Building PDF from TeX..."
+safe_cd "$TEMPLATE"
+latexmk -pdf -f --interaction=batchmode index.tex || exit 1
+
+# Copy files
+blue_echo "Copying the result to output..."
+mkdir -p "$OUTPUT"
+cp index.pdf "$OUTPUT/bachelors-thesis.pdf"
+
+# Cleanup (again)
+blue_echo "Cleaning up..."
+cleanup
--- src/thesis.textbundle/text.md
+++ src/thesis.textbundle/text.md
@@ -1,11 +1,3 @@
-# Acknowledgements
-
-This is the acknowledgements.
-
-# Contents
-
-This is the TOC.
-
 # Chapter 1
 
 This is Chapter 1.
@@ -15,13 +7,22 @@ Here is one of my marvellous pictures.
 
 Where is the freaking caption?
 
-# Chapter 2
+# Chapter 2 {#sec:ch2}
 
 This is Chapter 2.
 
 # Chapter 3
 
 This is Chapter 3.
+
+| Type     | Count |  ø Time | Median Time | ø Commits |
+| -------- | ----: | ------: | ----------: | --------: |
+| Atom     |     9 | 1:30:23 |     0:48:00 |      13,2 |
+| Molecule |    10 | 1:20:12 |     1:12:45 |       8,9 |
+| Layout   |     5 | 1:21:48 |     0:57:00 |       3,0 |
+| Template |     5 | 2:30:30 |     2:27:00 |       4,0 |
+
+: Average Times for Components {#tbl:table1}
 
 # Chapter 4
 
